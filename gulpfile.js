ef78e6d6204052c91a5d861724e7bbee6fc7097e
Velu <velu_developer@yahoo.com>
2017-02-23 09:29:57 +0530

    Inject-Reload task added

--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,98 @@
-const gulp = require('gulp');
-const concat = require('gulp-concat');
-const browserSync = require('browser-sync').create();
+var gulp 	= require('gulp'),
+	concat 	= require('gulp-concat'),
+	uglify 	= require('gulp-uglify'),
+	del 	= require('del'),
+	browserSync = require('browser-sync'),
+	browserSyncSpa = require('browser-sync-spa'),
+	mainBowerFiles = require('main-bower-files'),
+	appCssFiles = './src/**/*.css',
+	appJsFiles = './src/**/*.js',
+	indexFile = './src/index.html';
 
+/**
+ * CSS tasks
+ */
+gulp.task('css:vendor', function(){
+	gulp.src(mainBowerFiles(), {read: false})
+		.pipe(concat('vendor.css'))
+		.pipe(gulp.dest('dist/css'))
+		.pipe(browserSync.reload({stream: true}));
+})
 
-// Some pointless comments for our project.
-
-var devMode = false;
-
-gulp.task('css', function() {
-    gulp.src([
-            "./src/**/*.css"
-        ])
-        .pipe(concat('main.css'))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+gulp.task('css:app', function() {
+	gulp.src(appCssFiles)
+		.pipe(concat('app.css'))
+		.pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('js', function() {
-    gulp.src([
-            "./src/**/*.js"
-        ])
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('./dist/js'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+/**
+ * JS tasks
+ */
+gulp.task('js:vendor', function() {
+	gulp.src(mainBowerFiles(), { read: false })
+		/*.pipe(uglify().on('error', function(e){
+            console.log(e);
+        }))*/
+		.pipe(concat('vendor.js'))
+		.pipe(gulp.dest('./dist/js'));
 });
 
+gulp.task('js:app', function() {
+	gulp.src(appJsFiles)
+		.pipe(uglify().on('error', function(e){
+            console.log(e);
+        }))
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest('./dist/js'));
+});
+
+/**
+ * HTML tasks
+ */
 gulp.task('html', function() {
-    return gulp.src('./src/**/*.html')
-        .pipe(gulp.dest('./dist/'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+	return gulp.src(indexFile)
+		.pipe(gulp.dest('./dist/'))
+		.pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('inject', function () {
-  var target = gulp.src('./src/index.html');
-  // It's not necessary to read the files (will speed up things), we're only after their paths: 
-  var sources = gulp.src([
-    "./bower_components/**/*.min.js",
-    './src/**/*.js',
-    "./bower_components/**/*.min.css",
-    './src/**/*.css'
-    ], {read: false}
-  );
- 
-  return target.pipe(inject(sources))
-    .pipe(gulp.dest('./src'));
+/**
+ * BUILD tasks
+ */
+gulp.task('clean', function(cb){
+	del([
+		'dist/**'
+	], cb);
 });
 
-gulp.task('build', function() {
-    gulp.start(['css', 'js', 'html'])
+gulp.task('css',function() {
+	gulp.start(['css:vendor','css:app'])
+});
+
+gulp.task('js',function() {
+	gulp.start(['js:vendor','js:app'])
+});
+
+gulp.task('build',function() {
+	gulp.start(['css', 'js', 'html'])
 });
 
 gulp.task('browser-sync', function() {
-    browserSync.init(null, {
-        open: false,
-        server: {
-            baseDir: 'dist',
-        }
-    });
+	browserSync.init(null, {
+		open: true,
+		server: {
+			baseDir: 'dist',
+		}
+	});
+});
+
+gulp.task('inject-reload', ['build'], function ()
+{
+    browserSync.reload();
 });
 
 gulp.task('start', function() {
-    devMode = true;
-    gulp.start(['build', 'browser-sync']);
-    gulp.watch(['./src/**/*.css'], ['css']);
-    gulp.watch(['./src/**/*.js'], ['js']);
-    gulp.watch(['./src/**/*.html'], ['html']);
+	gulp.start(['build', 'browser-sync']);
+	gulp.watch(['./src/**/*.css'], ['inject-reload']);
+	gulp.watch(['./src/**/*.js'], ['inject-reload']);
+	gulp.watch(['./src/**/*.html'], ['html']);
 });
---
+++ src/app/index.module.js
@@ -0,0 +1,6 @@
+console.log('test1'); // test comment
+console.log('test2'); // test comment
+console.log('test3'); // test comment
+console.log('test4'); // test comment
+console.log('test5'); // test comment
+console.log('test6'); // test comment
